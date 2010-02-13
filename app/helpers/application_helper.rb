e425741cf3f39d9aa7fe1e27ea5f3d883f792f4b
mifan <mifanxp@gmail.com>
2010-02-13 15:38:37 +0800

    clean helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,13 +19,13 @@ module ApplicationHelper
   def auth_token
     "<input name=\"authenticity_token\" type=\"hidden\" value=\"#{form_authenticity_token}\" />"
   end
-  
+
   # return the Gravatar face by Email
   def face_url(email,size=128)
     hash = MD5::md5(email)
     "http://www.gravatar.com/avatar/#{hash}?s=#{size}"
   end
-  
+
   # check user have snippet's permission
   def has_permission(snippet)
     if @current_user
@@ -36,9 +36,9 @@ module ApplicationHelper
     
     return false
   end
-	
-	# format comment text
-	def format_comment(comment)
-		return auto_link(simple_format(html_escape(comment)))
-	end
+
+  # format comment text
+  def format_comment(comment)
+    return auto_link(simple_format(html_escape(comment)))
+  end
 end
